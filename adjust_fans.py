d2444b258a54e49ff4c9a7aaa5e2f7e17518634f
Casey <CaseysZ28@gmail.com>
2022-12-05 10:47:51 -0600

    added monitoring as well as a couple of strategies.  adding script to start re-write as well

--- adjust_fans.py
+++ adjust_fans.py
@@ -1,9 +1,13 @@
 """control comander pro and corsair hydro fan speeds"""
 
 import argparse
+import datetime
 import logging
 import os
+import sqlite3
 import time
+
+from sqlite3 import Error
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--speed", help="the percentage of max speed for fans")
@@ -13,7 +17,9 @@ parser.add_argument("--fan", help="the fan identifier to set to percentage")
 parser.add_argument("--status", help="show fan speed status", default=False, dest="status", action="store_true")
 parser.add_argument("--gaming", help="85% rad 70% case", default=False, dest="gaming", action="store_true")
 parser.add_argument("--quiet", help="60% rad 40% case", default=False, dest="quiet", action="store_true")
+parser.add_argument("--dynamic", help="dynamically set fan speeds based off AIO temp", default=False, dest="dynamic_speed", action="store_true")
 parser.add_argument("--monitor", help="watch AIO temp", default=False, dest="monitor", action="store_true")
+parser.add_argument("--interval", help="interval in which to refresh monitor command")
 args = parser.parse_args()
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(message)s')
@@ -21,45 +27,66 @@ logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s: %(
 aio_fans = [ "fan1", "fan2", "fan3" ]
 commander_fans = [ "fan1", "fan2", "fan3", "fan4", "fan5", "fan6" ]
 
+def set_fan_speed(device: str, fan: str, speed: int):
+    os.system(f'liquidctl --match "{device}" set {fan} speed {speed}')
+
+def set_aio_fan_speed(fan: str, speed: int):
+    set_fan_speed("Hydro", fan, speed)
+
+def set_commander_fan_speed(fan: str, speed:int):
+    set_fan_speed("Commander", fan, speed)
+
+def get_status():
+    os.system('liquidctl status')
+
 if args.monitor is True:
     try:
+        interval = args.interval
+        if args.interval is None:
+            interval = 10
+
         while args.monitor:
-            os.system('liquidctl status | grep Liquid | awk \'{print $4 $5}\'')
-            time.sleep(10)
-    except:
+            temp = os.popen('liquidctl status | grep Liquid | awk \'{print $4 $5}\'').read().strip()
+            ct = datetime.datetime.now().isoformat(timespec='seconds')
+            print(f'{ct} - {temp}')
+            time.sleep(int(interval))
+    except KeyboardInterrupt:
         print('\nmonitor canceled')
 
 if args.gaming is True:
     for fan in aio_fans:
-        os.system(f'liquidctl --match "Hydro" set {fan} speed 85')
+        set_aio_fan_speed(fan, 85)
     for fan in commander_fans:
-        os.system(f'liquidctl --match "Commander" set {fan} speed 70')
+        set_commander_fan_speed(fan, 70)
+    time.sleep(10)
+    get_status()
 
 if args.quiet is True:
     for fan in aio_fans:
-        os.system(f'liquidctl --match "Hydro" set {fan} speed 60')
+        set_aio_fan_speed(fan, 60)
     for fan in commander_fans:
-        os.system(f'liquidctl --match "Commander" set {fan} speed 40')
+        set_commander_fan_speed(fan, 40)
+    time.sleep(10)
+    get_status()
 
 if args.status is True:
     os.system(f'liquidctl status')
 
 if args.device == "AIO" or args.all_fans is True:
-    if args.all_fans is True:
-        for fan in aio_fans:
-            os.system(f'liquidctl --match "Hydro" set {fan} speed {args.speed}')
-    elif args.fan is not None:
-        os.system(f'liquidctl --match "Hydro" set fan{args.fan} speed {args.speed}')
+    if args.fan is not None:
+        set_aio_fan_speed(args.fan, args.speed)
     else:
         for fan in aio_fans:
-            os.system(f'liquidctl --match "Hydro" set {fan} speed {args.speed}')
+            set_aio_fan_speed(fan, args.speed)
+    time.sleep(10)
+    get_status()
 
 if args.device == "Commander" or args.all_fans is True:
-    if args.all_fans is True:
-        for fan in commander_fans:
-            os.system(f'liquidctl --match "Commander" set {fan} speed {args.speed}')
-    elif args.fan is not None:
-        os.system(f'liquidctl --match "Commander" set fan{args.fan} speed {args.speed}')
+    if args.fan is not None:
+        set_commander_fan_speed(args.fan, args.speed)
     else:
         for fan in commander_fans:
-            os.system(f'liquidctl --match "Commander" set {fan} speed {args.speed}')
+            set_commander_fan_speed(fan, args.speed)
+    time.sleep(10)
+    get_status()
+
